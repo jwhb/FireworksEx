5d6e389b1ee4b5987fe29424ece3a3fa7b58ccd6
Jasper Wiegratz <jwhy@jwhy.de>
2012-12-25 05:40:39 +0100

    Added abstraction model "FireworkStyle" for firework effects
    Signed-off-by: JWhy <jwhydev@jwhy.de>

---
+++ src/main/java/de/jwhy/fireworksex/FireworkStyle.java
@@ -0,0 +1,14 @@
+package de.jwhy.fireworksex;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+
+public class FireworkStyle {
+	
+	public Color color = null;
+	public FireworkEffect.Type shape = null;
+	public boolean trail = false;
+	public boolean flicker = false;
+	public long power = 1;
+	
+}
