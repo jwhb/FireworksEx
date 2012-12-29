9349d88238edca5a765b0232b3af082eb73f3d64
Jasper Wiegratz <jwhy@jwhy.de>
2012-12-29 11:51:03 +0100

    Improving FireworksExHelp and adding InvalidStyle message
    Signed-off-by: JWhy <jwhydev@jwhy.de>

--- src/main/java/de/jwhy/fireworksex/FireworksExHelp.java
+++ src/main/java/de/jwhy/fireworksex/FireworksExHelp.java
@@ -4,21 +4,48 @@ import org.bukkit.ChatColor;
 
 public class FireworksExHelp {
 
+	private static ChatColor o = ChatColor.GOLD;
+	private static ChatColor n = ChatColor.RESET;
+	private static ChatColor r = ChatColor.DARK_RED;
+	private static ChatColor y = ChatColor.YELLOW;
+	
+	private static String getPluginNameFormatted(){
+		String msg = o+"Fireworks"+r+"Ex";
+		return(msg);
+	}
+	
 	public static String getGenericHeader(){
 		ChatColor g = ChatColor.GREEN;
-		ChatColor o = ChatColor.GOLD;
-		ChatColor r = ChatColor.DARK_RED;
-		return(g+"============["+o+"Fireworks"+r+"Ex"+g+"]============");
+		String name = FireworksExHelp.getPluginNameFormatted();
+		return(g + "============[" + name + g + "]============");
 	}
 	
 	public static String[] getNoArgsHelp(String label){
 		ChatColor g = ChatColor.GRAY;
-		ChatColor o = ChatColor.GOLD;
-		ChatColor n = ChatColor.RESET;
-		String[] s = new String[2];
-		s[0] = FireworksExHelp.getGenericHeader();
-		s[1] = o + "Commands: /" + label + " shoot <style> - " + n + "Shoot rocket of predefined style";
-		return(s);
+		String[] msg = new String[2];
+		msg[0] = FireworksExHelp.getGenericHeader();
+		msg[1] = g + "Commands: " + o + "/" + label + " shoot <style> - " + n 
+				+ "Shoot rocket of predefined style";
+		return(msg);
+	}
+
+	public static String getInvalidStyle(String style) {
+		String name = FireworksExHelp.getPluginNameFormatted();
+		String stylepad;
+		if(!style.isEmpty()){
+			//Return style with padding
+			stylepad = y + style + " ";
+		}else{
+			//Return nothing with padding
+			stylepad = "";
+		}
+		String msg = name + n + " The requested style " + stylepad  + "could not be found!";
+
+		return(msg);
+	}
+	
+	public static String getInvalidStyle(){
+		return(FireworksExHelp.getInvalidStyle(""));
 	}
 	
 }
